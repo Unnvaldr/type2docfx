dd66a920c2f356feb6dcd7d8506b11cf586ad0fb
Martin <7252614+Lhoerion@users.noreply.github.com>
2020-10-18 23:13:33 +0200

    Change Yaml file header

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
 export const constructorName = 'constructor';
-export const yamlHeader = '### YamlMime:UniversalReference';
+export const yamlHeader = '### YamlMime:TypeScriptReference';
 export const setOfTopLevelItems = new Set(['class', 'interface', 'module']);
 export const langs = ['typeScript'];
 export const releaseStages = ['internal', 'alpha', 'beta', 'public'];
