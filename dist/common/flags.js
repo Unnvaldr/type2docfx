669bfe85366dc70a23f5c87319f5470802769545
Martin <7252614+Unnvaldr@users.noreply.github.com>
2022-08-27 01:18:04 +0200

    Clean dist folder

--- dist/common/flags.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.flags = void 0;
-exports.flags = {
-    hasModule: false,
-    enableAlphabetOrder: true,
-    tocAlphabetOrderOnly: false
-};
-//# sourceMappingURL=flags.js.map
